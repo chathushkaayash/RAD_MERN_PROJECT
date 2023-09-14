4b480ba965c3ce235ec1b2f449819870291f0422
chathushkaayash <chathushkaayash@gmail.com>
2023-09-15 03:17:13 +0530

    add users

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const connectDB = async ()=>{
         }
         
     catch(error){
-        console.error('MongoDB connection FAIL');
+        console.error('MongoDB connection FAILs');
         process.exit(1);
     }
 }
