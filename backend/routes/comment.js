49771ed9173064597ea77ad77265f4ac2809cf8a
AkilaDemotte <akilamilindademotte@gmail.com>
2023-09-15 03:12:54 +0530

    test

--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -8,24 +8,27 @@ const verifyToken = require('../verifyToken')
 
 
 //create
-router.post("/create",verifyToken,async (req,res)=>{
-    try{
+router.post("/create", verifyToken, async (req, res) => {
+    try {
+
+
         const newComment = new Post(req.body);
+
         const savedComment = await newComment.save();
         res.status(200).json(savedComment);
     }
-    catch(err){
+    catch (err) {
         res.status(500).json(err)
     }
 })
 
 //Update
-router.put("/:id",verifyToken,async (req,res)=>{
-    try{
-        const updatedComment=await Comment.findByIdAndUpdate(req.params.id,{$set:req.body},{new:true})
+router.put("/:id", verifyToken, async (req, res) => {
+    try {
+        const updatedComment = await Comment.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
         res.status(200).json(updatedComment);
     }
-    catch(err){
+    catch (err) {
         res.status(500).json(err)
     }
 })
@@ -33,12 +36,12 @@ router.put("/:id",verifyToken,async (req,res)=>{
 
 
 //delete comment
-router.delete("/:id",verifyToken,async (req,res)=>{
-    try{
+router.delete("/:id", verifyToken, async (req, res) => {
+    try {
         await Comment.findByIdAndDelete(req.params.id)
         res.status(200).json("Comment has been deleted");
     }
-    catch(err){
+    catch (err) {
         res.status(500).json(err)
     }
 })
@@ -46,12 +49,12 @@ router.delete("/:id",verifyToken,async (req,res)=>{
 
 
 //get  post comments
-router.get("/post/:postId",async (req,res)=>{
-    try{
-        const comments=await Comment.find({postId:req.params.postId})
+router.get("/post/:postId", async (req, res) => {
+    try {
+        const comments = await Comment.find({ postId: req.params.postId })
         res.status(200).json(comments);
     }
-    catch(err){
+    catch (err) {
         res.status(500).json(err)
     }
 })
