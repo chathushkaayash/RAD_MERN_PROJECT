a973266e37a9b118266b67b8e97c0b44a4eb272c
AkilaDemotte <akilamilindademotte@gmail.com>
2023-09-15 03:25:31 +0530

    test

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,15 @@ const express= require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
-
+const cors=require('cors')
+const authRoute = require('./routes/auth');
 const userRoute=require('./routes/users')
+const cookieParser=require('cookie-parser')
 const postRoute=require('./routes/post')
+const commentRoute=require('./routes/comment')
+
+
+
 app.use(cors({origin:"http://localhost:5173",credentials:true}))
 
 
@@ -22,9 +27,16 @@ const connectDB = async ()=>{
     }
 }
 
+//middlewares
 
+dotenv.config();
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth",authRoute);
 app.use("/api/users",userRoute);
 app.use("/api/posts",postRoute);
+app.use("/api/comments",commentRoute);
+
 
 
 
